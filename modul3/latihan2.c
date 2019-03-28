2c5162b678f4946b963e03ea957afe18f749105c
Hendra Ramadani <36802769+hendraramadani@users.noreply.github.com>
2019-03-28 18:03:57 +0700

    Update latihan2.c

--- modul3/latihan2.c
+++ modul3/latihan2.c
@@ -15,7 +15,7 @@ struct range{
 
 void* tulis(void* args){
     struct range*extract =(struct range*)args;
-    printf("%d %d\n",extract->low,extract->high);
+    printf("[ Range %d sampai %d ]\n",extract->low,extract->high);
 
     for(int i =extract->low; i <=extract->high; i++){
         int isPrime = 0;
@@ -31,37 +31,37 @@ void* tulis(void* args){
             prime_arr[i]= i;
         }
     }
+    for(int i=extract->low; i<extract->high; i++){ //print prime per thread
+        if(prime_arr[i]!=0){
+            printf("%d ",prime_arr[i]);
+        }
+   }
+   printf("\n");
 }
 
 
 
 
 
-int main(void){
-    int n;
-    int t;
+int main(){
+    int n,t;
     scanf("%d %d",&n, &t);
+    if(n==0||t==0){
+	printf("input > 0\n");
+	scanf("%d %d",&n, &t);
+    }
     struct range rentang;
     rentang.low=2;
     rentang.high=n/t;
 
-    for(int i=0;i<t-1; i++){
-        //printf("> %d %d\n",rentang.low,rentang.high);
+    for(int i=0;i<t; i++){
         pthread_create(&tid[i], NULL, &tulis,(void *)&rentang);
         pthread_join(tid[i],NULL);
         rentang.low=1+rentang.high;
         rentang.high+=(n/t);
     }
 
-    //ini untuk sisanya.terakhir ,misal 10 3 itu jadi {3 3 4},4 diambil terakhir.
-    rentang.high=n;
-    pthread_create(&tid[t-1], NULL, &tulis,(void *)&rentang);
-    pthread_join(tid[t-1],NULL);
 
-    for(int i=2; i<n; i++){
-        if(prime_arr[i]!=0){
-            printf("%d\n",prime_arr[i]);
-        }
-    }
+    
     return 0;
 }
